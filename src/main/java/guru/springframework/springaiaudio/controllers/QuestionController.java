e7d573d1d543ef36ce84f8c02af847576662b87c
John Thompson <john@springframework.guru>
2024-04-17 15:17:28 -0400

    removing method

--- src/main/java/guru/springframework/springaiaudio/controllers/QuestionController.java
+++ src/main/java/guru/springframework/springaiaudio/controllers/QuestionController.java
@@ -1,12 +1,8 @@
 package guru.springframework.springaiaudio.controllers;
 
 
-import guru.springframework.springaiaudio.model.Question;
 import guru.springframework.springaiaudio.services.OpenAIService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,9 +14,6 @@ public class QuestionController {
 
     private final OpenAIService openAIService;
 
-    @PostMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getImage(@RequestBody Question question) {
-        return null;
-    }
+    //todo implement this
 
 }
